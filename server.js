33a0ce2f8a1af15931a832cbbf77c7b09760fc99
RaniaAwny <105688725+RaniaAwny@users.noreply.github.com>
2023-02-12 11:31:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
+// Setup empty JS object to act as endpoint for all routes
+
 projectData = {};
 
 // Require Express to run server and routes
+
 const express = require("express");
+
 // Start up an instance of app
+
 const app = express();
 
 // Dependencies
+
 const bodyParser = require("body-parser");
 
 /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(body-parser.urlencoded({ extended: false }));
-app.use(body-parser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 // cors for cross origin allowance
+
 const cors = require("cors");
+
 app.use(cors());
 
 // Initialize the main project foldernpm
+
 app.use(express.static("website"));
 
 // Setup Server
-const port = 3000;
+
+const port = 4000;
 
 // set variable named server and pass the listen method with two arrgument
+
 const server = app.listen(port, listening);
+
 function listening() {
   console.log(`server running`);
+
   console.log(`server is running on localhost: ${port}`);
 }
 
 // respose from the server when a Get route
+
 app.get("/getWeather", (req, res) => {
   res.send(projectData);
 });
